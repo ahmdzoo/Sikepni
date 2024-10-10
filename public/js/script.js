86d4c4b919719501dd352c4af20a4fe4049b2ce9
lar <kokoapriliyantama@gmail.com>
2024-10-10 12:42:56 +0700

    nav menu

--- public/js/script.js
+++ public/js/script.js
@@ -61,42 +61,19 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
+  /*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+          nav = document.getElementById(navId)
+ 
+    toggle.addEventListener('click', () =>{
+        // Add show-menu class to nav menu
+        nav.classList.toggle('show-menu')
+ 
+        // Add show-icon to show and hide the menu icon
+        toggle.classList.toggle('show-icon')
+    })
+ }
+ 
+ showMenu('nav-toggle','nav-menu')
 
-// Modal Image Gallery
-function onClick(element) {
-    document.getElementById("img01").src = element.src;
-    document.getElementById("modal01").style.display = "block";
-    var captionText = document.getElementById("caption");
-    captionText.innerHTML = element.alt;
-  }
-  
-
-  var mySidebar = document.getElementById("mySidebar");
-  
-  function w3_open() {
-    if (mySidebar.style.display === 'block') {
-      mySidebar.style.display = 'none';
-    } else {
-      mySidebar.style.display = 'block';
-    }
-  }
-  
-  function w3_close() {
-      mySidebar.style.display = "none";
-  }
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   const navToggle = document.getElementById("nav-toggle");
-//   const navMenu = document.getElementById("nav-menu");
-//   const navBurger = document.querySelector(".nav__burger");
-//   const navClose = document.querySelector(".nav__close");
-
-//   navToggle.addEventListener("click", () => {
-//     // Toggle menu visibility
-//     navMenu.classList.toggle("show-menu"); // Toggle class to show/hide menu
-
-//     // Toggle icons
-//     navBurger.classList.toggle("hide-icon"); // Hide burger icon
-//     navClose.classList.toggle("show-icon");  // Show close icon
-//   });
-// });
