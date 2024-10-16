4064c5b5025fda38b416b055ad4bf829b6bcf1f2
teguhf <teguhef10@gmail.com>
2024-10-16 10:33:18 +0700

    penambahan footer di semua dashboard

--- resources/views/mahasiswa/mhs_lowongan.blade.php
+++ resources/views/mahasiswa/mhs_lowongan.blade.php
@@ -114,5 +114,5 @@
 <!-- Bootstrap JS -->
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-
+@include('layouts/footer')
 @endsection
