d9812cd1697327d8e4862ac472b1de04705dc2b6
ahmdzoo <ahmdzoo>
2024-10-22 12:23:56 +0700

    selasa

--- resources/views/mahasiswa/mhs_lowongan.blade.php
+++ resources/views/mahasiswa/mhs_lowongan.blade.php
@@ -104,5 +104,8 @@
       <!-- Bootstrap JS -->
       <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-      @include('layouts/footer')
-      @endsection
+    </div>
+  </div>
+</div>
+@include('layouts/footer')
+@endsection
